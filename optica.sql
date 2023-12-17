1d1314697d04eb967c15dc63f3c02b8e6a5bf223
Reivax9696 <149475827+Reivax9696@users.noreply.github.com>
2023-12-17 20:02:18 +0100

    Update optica.sql

--- optica.sql
+++ optica.sql
@@ -3,28 +3,28 @@ USE optica;
 
 CREATE TABLE proveidors (
     proveidor_id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255),
-    adreca_carrer VARCHAR(255),
-    adreca_numero VARCHAR(255),
-    adreca_pis VARCHAR(255),
-    adreca_porta VARCHAR(255),
-    ciutat VARCHAR(255),
-    codi_postal VARCHAR(255),
-    pais VARCHAR(255),
-    telefon VARCHAR(255),
-    fax VARCHAR(255),
-    NIF VARCHAR(255)
+    nom VARCHAR(50),
+    adreca_carrer VARCHAR(50),
+    adreca_numero VARCHAR(50),
+    adreca_pis VARCHAR(50),
+    adreca_porta VARCHAR(50),
+    ciutat VARCHAR(50),
+    codi_postal VARCHAR(50),
+    pais VARCHAR(50),
+    telefon VARCHAR(20),
+    fax VARCHAR(50),
+    NIF VARCHAR(50)
 );
 
 CREATE TABLE ulleres (
     ulleres_id INT AUTO_INCREMENT PRIMARY KEY,
-    marca VARCHAR(255),
+    marca VARCHAR(50),
     graduacio_vidre_dreta FLOAT,
     graduacio_vidre_esquerra FLOAT,
-    tipus_montura VARCHAR(255),
-    color_montura VARCHAR(255),
-    color_vidre_esquerra VARCHAR(255),
-    color_vidre_dreta VARCHAR(255),
+    tipus_montura VARCHAR(50),
+    color_montura VARCHAR(50),
+    color_vidre_esquerra VARCHAR(50),
+    color_vidre_dreta VARCHAR(50),
     preu FLOAT,
     proveidor_id INT,
     FOREIGN KEY (proveidor_id) REFERENCES proveidors(proveidor_id)
@@ -32,10 +32,10 @@ CREATE TABLE ulleres (
 
 CREATE TABLE clients (
     client_id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255),
-    adreca_postal VARCHAR(255),
-    telefon VARCHAR(255),
-    correu_electronic VARCHAR(255),
+    nom VARCHAR(50),
+    adreca_postal VARCHAR(50),
+    telefon VARCHAR(20),
+    correu_electronic VARCHAR(50),
     data_registre DATE,
     client_recomanador_id INT,
     FOREIGN KEY (client_recomanador_id) REFERENCES clients(client_id)
@@ -43,7 +43,7 @@ CREATE TABLE clients (
 
 CREATE TABLE empleats (
     empleat_id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255)
+    nom VARCHAR(50)
 );
 
 CREATE TABLE vendes (
@@ -52,7 +52,7 @@ CREATE TABLE vendes (
     empleat_id INT,
     ulleres_id INT,
     data_venda DATE,
-    periode_vendes VARCHAR(255),
+    periode_vendes VARCHAR(50),
     FOREIGN KEY (client_id) REFERENCES clients(client_id),
     FOREIGN KEY (empleat_id) REFERENCES empleats(empleat_id),
     FOREIGN KEY (ulleres_id) REFERENCES ulleres(ulleres_id)
