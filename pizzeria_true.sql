4c479f985e1bd74ae8c9755eaaf96952f6935187
ModeRed55 <42684549+ModeRed55@users.noreply.github.com>
2024-03-03 23:00:53 +0100

    commit3

--- pizzeria_true.sql
+++ pizzeria_true.sql
@@ -5,11 +5,11 @@ CREATE TABLE clients (
     client_id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50),
     cognoms VARCHAR(50),
-    adresa VARCHAR(50),
-    codi_postal VARCHAR(10),
+    adresa VARCHAR(100), 
+    codi_postal CHAR(5), 
     localitat VARCHAR(50),
     provincia VARCHAR(50),
-    telefon VARCHAR(20)
+    telefon VARCHAR(10) 
 );
 
 CREATE TABLE categories (
@@ -21,7 +21,7 @@ CREATE TABLE productes (
     producte_id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50),
     descripcio TEXT,
-    imatge VARCHAR(50),
+    imatge VARCHAR(255),
     preu FLOAT,
     categoria_id INT,
     FOREIGN KEY (categoria_id) REFERENCES categories(categoria_id)
@@ -29,8 +29,8 @@ CREATE TABLE productes (
 
 CREATE TABLE botigues (
     botiga_id INT AUTO_INCREMENT PRIMARY KEY,
-    adresa VARCHAR(50),
-    codi_postal VARCHAR(10),
+    adresa VARCHAR(100), 
+    codi_postal CHAR(5), 
     localitat VARCHAR(50),
     provincia VARCHAR(50)
 );
@@ -39,32 +39,26 @@ CREATE TABLE comandes (
     comanda_id INT AUTO_INCREMENT PRIMARY KEY,
     client_id INT,
     data_hora DATETIME,
-    tipus_entrega VARCHAR(50),
+    tipus_entrega ENUM('A domicili', 'Recollida en botiga'), 
     preu_total FLOAT,
-    botiga_id INT,
+    botiga_id INT, 
+    empleat_repartidor_id INT NULL, 
     FOREIGN KEY (client_id) REFERENCES clients(client_id),
-    FOREIGN KEY (botiga_id) REFERENCES botigues(botiga_id)
+    FOREIGN KEY (botiga_id) REFERENCES botigues(botiga_id),
+    FOREIGN KEY (empleat_repartidor_id) REFERENCES empleats(empleat_id) 
 );
 
 CREATE TABLE empleats (
     empleat_id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50),
     cognoms VARCHAR(50),
-    NIF VARCHAR(20),
-    telefon VARCHAR(20),
-    rol VARCHAR(50),
+    NIF VARCHAR(9), 
+    telefon VARCHAR(10), 
+    rol ENUM('Manager', 'Cuiner', 'Repartidor'), 
     botiga_id INT,
     FOREIGN KEY (botiga_id) REFERENCES botigues(botiga_id)
 );
 
-CREATE TABLE repartiment_detalls (
-    comanda_id INT,
-    empleat_id INT,
-    data_hora_lliurament DATETIME,
-    FOREIGN KEY (comanda_id) REFERENCES comandes(comanda_id),
-    FOREIGN KEY (empleat_id) REFERENCES empleats(empleat_id),
-    PRIMARY KEY (comanda_id, empleat_id)
-);
 
 CREATE TABLE comanda_detalls (
     comanda_id INT,
